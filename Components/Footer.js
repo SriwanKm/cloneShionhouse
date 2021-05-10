091606ec173258c4e1c4b338a56c44465608e3be
SriwanKm <kcqkoo@gmail.com>
2021-05-09 20:25:46 -0700

    add footer details

--- Components/Footer.js
+++ Components/Footer.js
@@ -23,6 +23,33 @@ export default function Footer() {
                     <FontAwesome name="pinterest-p" size={22} color="white"/>
                 </TouchableOpacity>
             </View>
+            <View>
+                <Text style={styles.subHeadFooter}>Quick links</Text>
+                <TouchableOpacity><Text style={styles.subFooterText}>Image Licensin</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>Style Guide</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>Privacy Policy</Text></TouchableOpacity>
+
+                <Text style={styles.subHeadFooter}>Shop Category</Text>
+                <TouchableOpacity><Text style={styles.subFooterText}>Image Licensin</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>Style Guide</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>Privacy Policy</Text></TouchableOpacity>
+
+                <Text style={styles.subHeadFooter}>Partners</Text>
+                <TouchableOpacity><Text style={styles.subFooterText}>(89) 982-278 356</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>demo@colorlib.com</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>67/A, Colorlib, Green road,
+                    NYC</Text></TouchableOpacity>
+
+                <Text style={styles.subHeadFooter}>Get in touch</Text>
+                <TouchableOpacity><Text style={styles.subFooterText}>Image Licensin</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>Style Guide</Text></TouchableOpacity>
+                <TouchableOpacity><Text style={styles.subFooterText}>Privacy Policy</Text></TouchableOpacity>
+            </View>
+            <View style={{alignItems: 'center', marginTop: 50}}>
+                <Text style={[styles.footerText, {textAlign: 'center'}]}>
+                    Copyright ©2021 All rights reserved | This template is made with by <Text style={{color:'#9F79FF'}}>Colorlib</Text>
+                </Text>
+            </View>
         </View>
     )
 }
@@ -60,7 +87,22 @@ const styles = ScaledSheet.create(
         },
         footerText: {
             color: '#C2C5DB',
-            fontSize: '14@s'
+            lineHeight: '25@s',
+            marginBottom: '25@s',
+
         },
+        subHeadFooter: {
+            fontSize: '16@s',
+            color: 'white',
+            letterSpacing: 0.5,
+            fontWeight: 'bold',
+            marginTop: '20@s',
+            marginBottom: '10@s',
+
+        },
+        subFooterText: {
+            lineHeight: '28@s',
+            color: '#C2C5DB',
+        }
     }
 )
